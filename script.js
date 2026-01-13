0a6fba797bfa4711aad86e77fde5b987d870e105
Edgar AguilarGavilan <edgara70@nycstudents.net>
2026-01-13 14:39:29 -0500

    Finished Dom mini project 2

--- script.js
+++ script.js
@@ -7,31 +7,49 @@ let nebula_spot = 1;
 let comet_spot  = 1;
 
 // Query Selectors
-const apolloBtn   = 
-const nebulaBtn   = 
-const cometBtn    = 
+const apolloBtn   = document.querySelector("#apollo-btn");
+const nebulaBtn   = document.querySelector("#nebula-btn");
+const cometBtn    = document.querySelector("#comet-btn");
 
-const apolloRocket = 
-const nebulaRocket = 
-const cometRocket  = 
+const apolloRocket = document.querySelector("#apollo-rocket");
+const nebulaRocket = document.querySelector("#nebula-rocket");
+const cometRocket  = document.querySelector("#comet-rocket");
 
-const winnerBox = 
+const winnerBox = document.querySelector("#winner");
 
 // Click Handlers
-const boostApollo = () => {
-  
+const boostApollo = (e) => {
+console.log("clicked");
+apollo_spot += 1;
+moveTo(apolloRocket, apollo_spot);
+checkWinner(apollo_spot, "Apollo");
+apolloRocket.alt = `apollo rocket at spot ${apollo_spot} out of 5`;
 };
 
-const boostNebula = () => {
-
+const boostNebula = (e) => {
+console.log("clicked");
+nebula_spot += 1;
+moveTo(nebulaRocket, nebula_spot);
+checkWinner(nebula_spot, "Nebula");
+nebulaRocket.alt = `nebula rocket at spot ${nebula_spot} out of 5`;
 };
 
-const boostComet = () => {
-
+const boostComet = (e) => {
+console.log("clicked");
+comet_spot += 1;
+moveTo(cometRocket, comet_spot);
+checkWinner(comet_spot, "Comet");
+cometRocket.alt = `comet rocket at position ${comet_spot} out of 5`;
 };
 
 // Winner Check
-
+const checkWinner = (spot, rocket) => {
+    if (spot === 6) {
+        winnerBox.innerHTML = rocket + " is the Winner!";
+    }
+};
 
 // Events
-
+apolloBtn.addEventListener("click", boostApollo);
+nebulaBtn.addEventListener("click", boostNebula);
+cometBtn.addEventListener("click", boostComet);
